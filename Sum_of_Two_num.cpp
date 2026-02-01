b54de08c9ece52021907331462caba7773a5f9e9
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2026-02-01 12:09:54 +0530

    Sum of 2 numbers a and b

---
+++ Sum_of_Two_num.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int sum;
+
+    cout << "Enter the two numbers: ";
+    cin >> a >> b;
+    sum = a + b;
+    cout << "Sum of the two numbers is: " << sum << endl;
+
+    return 0;
+}
+
+// This program takes two integers as input from the user and outputs their sum.
