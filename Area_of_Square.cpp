a831b4d52ce888a9c6be73649fe313ab78ee5b1a
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2026-02-02 23:52:07 +0530

    area of square

---
+++ Area_of_Square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int side;
+    cout << "Enter the side of the square: ";
+    cin >> side;
+    int area = side * side;
+    cout << "Area of the square is: " << area << endl;
+    
+    return 0;
+}
+
+// This program takes the length of a side of a square as input and outputs its area.
