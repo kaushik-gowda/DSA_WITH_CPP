7a601d1afcb8de1ab84630381c19717526ad07a0
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2026-02-04 21:32:06 +0530

    even print

---
+++ ODD_EVEN_NUMBERS/Print_EVEN_Num.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    cout << "Even numbers from 1 to " << n << " are: ";
+    for (int i =0; i <= n; i++) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
+
+// This code prints all even numbers from 1 to N, where N is a positive integer input by the user.
