e0323f4b4a2d4648f00ed9f68e976f07b261d4bb
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2026-02-05 00:15:59 +0530

    pattern 4

---
+++ PATTERNS/Pattern_4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    int m = 1;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << m << " ";
+            m++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// Sample Input/Output:
+// Enter the number of rows: 4
+// 1 2 3 4
+// 5 6 7 8
+// 9 10 11 12
+// 13 14 15 16
