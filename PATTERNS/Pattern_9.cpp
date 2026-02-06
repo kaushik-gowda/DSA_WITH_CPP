6fcdf05618459aa6655cb122aedc23bdb65de43d
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2026-02-06 11:18:50 +0530

    pattern 9

---
+++ PATTERNS/Pattern_9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    char ch = 'A';
+
+    for (int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << ch << " ";
+        }
+        ch++;
+        cout << endl;
+    }
+    return 0;
+}
+
+// Sample Input/Output:
+// Enter the number of rows: 4
+// A
+// B B
+// C C C
+// D D D D
