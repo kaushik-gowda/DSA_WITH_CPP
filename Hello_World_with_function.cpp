398ddbf92537cbd533d75f8456a053172bca175e
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2026-02-08 09:45:17 +0530

    Hello world

---
+++ Hello_World_with_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printHelloWorld() {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+
+    printHelloWorld();
+    return 0;
+}
+
+// Sample Output:
+// Hello, World!
