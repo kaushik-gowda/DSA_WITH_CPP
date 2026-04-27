46642097062e96fb6ac6cd06ac785a3fe100281e
kaushik-gowda <168765390+kaushik-gowda@users.noreply.github.com>
2026-04-27 22:07:48 +0530

    decremental

---
+++ POINTERS/Decremental_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int a = 10;
+    int* ptr = &a;
+
+    cout << ptr << endl;
+    ptr--;
+    cout << ptr << endl;
+    return 0;
+}
